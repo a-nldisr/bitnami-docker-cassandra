91561e78d9a129a65d9d9d01ccc461bd29336c70
Rogier Dikkes <1867776+a-nldisr@users.noreply.github.com>
2021-09-27 13:46:40 +0200

    Added prestop.sh script to drain nodes
    Signed-off-by: Rogier Dikkes <1867776+a-nldisr@users.noreply.github.com>

---
+++ 3/debian-10/rootfs/opt/bitnami/scripts/cassandra/prestop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purpose
+
+info "** Stopping Cassandra, running nodetool drain **"
+
+nodetool drain
